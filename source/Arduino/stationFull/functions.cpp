2be86e84b0e36683fb3f4d3694f66f32ec9a1a1c
Mirko Cardoso <cardoso.mirko@hotmail.it>
2017-09-12 14:46:00 +0200

    Set timezone to UTC (+0000)

--- source/Arduino/stationFull/functions.cpp
+++ source/Arduino/stationFull/functions.cpp
@@ -1,48 +1,36 @@
 
 #include "functions.h"
 
-String getFormattedDate(DateTime dt, const char timezone[])
+String getFormattedDate(DateTime dt)
 {
     char sz[30];
-    sprintf(sz, "%04d-%02d-%02dT%02d:%02d:%02d%s", dt.year(), dt.month(), dt.day(), dt.hour(), dt.minute(), dt.second(), timezone);
-    return String(sz);
+    sprintf(sz, "%04d-%02d-%02dT%02d:%02d:%02d", dt.year(), dt.month(), dt.day(), dt.hour(), dt.minute(), dt.second());
+    return String(sz) + String("+0000");
 }
 
 
 /**
  * Syncronize RTC
  */
-bool syncRTC(ICom& com, RTC_DS1307 rtc, char timezone[]) {
+bool syncRTC(ICom& com, RTC_DS1307 rtc) {
 
-    /*Serial.print(F("Sincronize RTC..."));
+    Serial.print(F("Sincronize RTC..."));
 
     uint8_t tmp = 0;
-    uint32_t* result = com.ntpUpdate("metasntp11.admin.ch", 8);
+    uint32_t* result = com.ntpUpdate("metasntp11.admin.ch", 0);
     while(result[0] < 2017 && tmp < 5)
     {
-        result = com.ntpUpdate("metasntp11.admin.ch", 8);
+        result = com.ntpUpdate("metasntp11.admin.ch", 0);
 
         tmp++;
         delay(1000);
     }
 
-    if (tmp >= 5)
+    if (tmp >= 5 && result[0] < 2017)
         return false;
 
 
     rtc.adjust(DateTime((uint16_t) result[0], (uint8_t) result[1], (uint8_t) result[2], (uint8_t) result[3], (uint8_t) result[4], (uint8_t) result[5]));
-
-    if ((bool)result[8] >= 0)
-        sprintf(timezone, "+%02d%02d", (int)result[6], (int)result[7]);
-    else
-        sprintf(timezone, "-%02d%02d", (int)result[6], (int)result[7]);
-    */
-
-    rtc.adjust(DateTime(2017, 7, 13, 9, 00, 0));
-    sprintf(timezone, "+%02d%02", 2, 0);
-
-
-    //delete[] result;
 
     return true;
 }
--- source/Arduino/stationFull/functions.h
+++ source/Arduino/stationFull/functions.h
@@ -6,8 +6,8 @@
 #include <com/comunication.h>
 
 
-String getFormattedDate(DateTime dt, const char timezone[]);
-bool syncRTC(ICom& com, RTC_DS1307 rtc, char timezone[]);
+String getFormattedDate(DateTime dt);
+bool syncRTC(ICom& com, RTC_DS1307 rtc);
 bool calcInterval(uint8_t current, uint8_t last, unsigned int interval);
 
 
